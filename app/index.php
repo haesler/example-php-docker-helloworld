abc723afd408d5919982877a200a7cddda64665c
haesler <haesler.marc@sunrise.ch>
2019-11-03 15:23:32 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -16,7 +16,7 @@
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
+ 
     <!-- Custom styles for this template -->
     <link href="css/starter-template.css" rel="stylesheet">
 
